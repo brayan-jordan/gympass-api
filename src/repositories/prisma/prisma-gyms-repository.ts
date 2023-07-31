e57b4d965376517f076e640e9d2349eed28d50c1
Brayan Vinicius Jordan <brayan.11fera@gmail.com>
2023-07-31 19:48:04 -0300

    Mapeando repositórios do prisma

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,46 @@
+import { Gym, Prisma } from '@prisma/client'
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaGymsRepository implements GymsRepository {
+  findById(id: string) {
+    const gym = prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return gym
+  }
+
+  create(data: Prisma.GymCreateInput) {
+    const gym = prisma.gym.create({
+      data,
+    })
+
+    return gym
+  }
+
+  searchMany(query: string, page: number) {
+    const gyms = prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<[Gym]>`
+        SELECT * from gyms
+        WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+    return gyms
+  }
+}
